5b891397004c92fb4c92c29a67a879b1de11c162
leohong1106 <52034865+leohong1106@users.noreply.github.com>
2019-07-25 09:00:59 +0900

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,9 @@
+select * from testdb2.user;
+
+insert into user (NAME, ADDR, PHONE) values ('이모씨', '안양','010-222');
+
+select * from user where user_seq = 11;
+
+insert into user (name) values ('김아무개');
+
+
